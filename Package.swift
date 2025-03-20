dde9be22792ee9ae52205483f2a372136df91ffe
Myrddin Krustowski <54777517+theundergroundsorcerer@users.noreply.github.com>
2025-03-20 12:25:09 +0200

    Updated Package.swift and added the Sources/Shapes/ShapesBouncingBall folder

--- Package.swift
+++ Package.swift
@@ -27,14 +27,12 @@ let package = Package(
         .executableTarget(
             name: "CoreInputMouse",
             dependencies: ["RaylibSwift"],
-            path: "Sources/Core/CoreInputMouse"
-        ),
+            path: "Sources/Core/CoreInputMouse"),
 
         .executableTarget(
             name: "CoreInputMouseWheel",
             dependencies: ["RaylibSwift"],
-            path: "Sources/Core/CoreInputMouseWheel"
-        ),
+            path: "Sources/Core/CoreInputMouseWheel"),
 
         .executableTarget(
             name: "CoreInputGamepad",
@@ -42,31 +40,31 @@ let package = Package(
             path: "Sources/Core/CoreInputGamepad",
             resources: [
                 .process("Resources")  // This includes the Resources directory with ps3.png and xbox.png
-            ]
-        ),
+            ]),
 
         .executableTarget(
             name: "CoreInputMutlitouch",
             dependencies: ["RaylibSwift"],
-            path: "Sources/Core/CoreInputMultitouch"
-        ),
+            path: "Sources/Core/CoreInputMultitouch"),
 
         .executableTarget(
             name: "CoreInputGestures",
             dependencies: ["RaylibSwift"],
-            path: "Sources/Core/CoreInputGestures"
-        ),
+            path: "Sources/Core/CoreInputGestures"),
 
         .executableTarget(
             name: "CoreInputVirtualControls",
             dependencies: ["RaylibSwift"],
-            path: "Sources/Core/CoreInputVirtualControls"
-        ),
+            path: "Sources/Core/CoreInputVirtualControls"),
         
         .executableTarget(
             name: "ShapesBasicShapes",
             dependencies: ["RaylibSwift"],
-            path: "Sources/Shapes/ShapesBasicShapes"
-        )
+            path: "Sources/Shapes/ShapesBasicShapes"),
+
+        .executableTarget(
+            name: "ShapesBouncingBall",
+            dependencies: ["RaylibSwift"],
+            path: "Sources/Shapes/ShapesBouncingBall")
     ]
 )
