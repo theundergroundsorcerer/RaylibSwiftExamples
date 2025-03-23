470a9aafca68d0c0fa8c18e611906b9f99ead838
Myrddin Krustowski <54777517+theundergroundsorcerer@users.noreply.github.com>
2025-03-23 23:31:36 +0200

    Removed Package.resolved

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ import PackageDescription
 
 let package = Package(
     name: "RaylibSwiftExamples",
-    dependencies: [.package(url: "https://github.com/theundergroundsorcerer/RaylibSwift.git", branch: "main")],
+    dependencies: [.package(url: "https://github.com/theundergroundsorcerer/RaylibSwift.git", branch: "develop")],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
